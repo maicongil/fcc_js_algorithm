4006a1c7ae80e672d761bb8bf220fbc7452ca694
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-01-16 16:40:10 -0200

    Reverse a String Solution

---
+++ basicAlgorithmScripting.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var BasicAlgorithmScripting;
+
+BasicAlgorithmScripting = {
+    reverseString : function(str){
+        return str.split('').reverse().join('');
+    }
+};
+
+module.exports = BasicAlgorithmScripting;
