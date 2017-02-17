9f33cb3c5bf6e40566fb6b8c6159746401e22b7f
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-02-17 10:51:04 -0200

    Exact Change solution

--- advancedAlgorithmScripting.js
+++ advancedAlgorithmScripting.js
@@ -3,7 +3,51 @@
 var AdvancedAlgorithmScripting;
 
 AdvancedAlgorithmScripting = {
+    checkCashRegister : function(price, cash, cid){
+        
+        var money = {
+                penny : 0.01,
+                nickel : 0.05,
+                dime : 0.10,
+                quarter : 0.25,
+                one : 1.00,
+                five : 5.00,
+                ten : 10.00,
+                twenty : 20.00,
+                onehundred : 100.00
+        };
+        
+        var changeDue = cash - price;
+        var change =[];
 
+        cid.reverse().forEach(function(item){
+            var currentCoinValue = money[item[0].trim().toLowerCase()];
+            if(changeDue >= currentCoinValue){
+                var coinAcc = 0.00;
+                while(((changeDue - currentCoinValue )>=0.00) && ((item[1] - currentCoinValue )>=0.00)){
+                    changeDue = (changeDue - currentCoinValue).toFixed(2);
+                    item[1] = (item[1] - currentCoinValue).toFixed(2);
+                    coinAcc += currentCoinValue;
+                }
+                change.push([item[0], coinAcc]);
+            }
+        });
+
+        var cashInDrawer = cid.reduce(function(a, b){
+            return a + b[1];
+        }, 0.00);
+
+
+        if(changeDue > 0.00){
+            return "Insufficient Funds";
+        }
+
+        if(cashInDrawer == 0.00){
+            return 'Closed';
+        }
+
+        return change;
+    }
 };
 
 module.exports = AdvancedAlgorithmScripting;
---
+++ exact_change_test.js
@@ -0,0 +1,54 @@
+var expect = require('chai').expect;
+var AdvancedAlgorithmScripting = require('./advancedAlgorithmScripting.js');
+
+describe('checkCashRegister()', function(){
+    
+    var actual = null;
+    var expected = null;
+
+    it('checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return an array.', function(){
+        actual= AdvancedAlgorithmScripting.checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+        expect(actual).to.be.instanceof(Array);
+    });
+
+    it('checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return a string.', function(){
+        actual= AdvancedAlgorithmScripting.checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+        expect(actual).to.be.a('string');
+    });
+
+    it('checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return a string.', function(){
+        actual = AdvancedAlgorithmScripting.checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+        expect(actual).to.be.a('string');
+    });
+
+    it('checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return [["QUARTER", 0.50]].', function(){
+        actual = AdvancedAlgorithmScripting.checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+        expected = [["QUARTER", 0.50]];
+        expect(actual).to.be.eql(expected);
+    });
+
+    it('checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return [["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", .50], ["DIME", 0.20], ["PENNY", 0.04]].', function(){
+        actual = AdvancedAlgorithmScripting.checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+        expected = [["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", .50], ["DIME", 0.20], ["PENNY", 0.04]];
+        expect(actual).to.be.eql(expected);
+    });
+
+    it('checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return "Insufficient Funds".', function(){
+        actual = AdvancedAlgorithmScripting.checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ;
+        expected = 'Insufficient Funds';
+        expect(actual).to.be.equal(expected);
+    });
+
+    it('checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return "Insufficient Funds".', function(){
+        actual = AdvancedAlgorithmScripting.checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+        expected = 'Insufficient Funds';
+        expect(actual).to.be.equal(expected);
+    });
+
+    it('checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return "Closed".', function(){
+        actual = AdvancedAlgorithmScripting.checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+        expected = 'Closed';
+        expect(actual).to.be.equal(expected);
+    });
+
+})
